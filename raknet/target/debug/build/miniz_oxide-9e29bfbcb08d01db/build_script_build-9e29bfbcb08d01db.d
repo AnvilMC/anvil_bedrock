bcc6bcb3f32f8f1e95a5581f9bb8ee5a3724cba0
besuper <16976439+besuper@users.noreply.github.com>
2021-03-29 23:17:07 +0200

    Updated gitignore

--- raknet/target/debug/build/miniz_oxide-9e29bfbcb08d01db/build_script_build-9e29bfbcb08d01db.d
+++
@@ -1,5 +0,0 @@
-C:\Users\ccgauche\Documents\GitHub\anvil_bedrock\raknet\target\debug\build\miniz_oxide-9e29bfbcb08d01db\build_script_build-9e29bfbcb08d01db.exe: C:\Users\ccgauche\.cargo\registry\src\github.com-1ecc6299db9ec823\miniz_oxide-0.4.4\build.rs
-
-C:\Users\ccgauche\Documents\GitHub\anvil_bedrock\raknet\target\debug\build\miniz_oxide-9e29bfbcb08d01db\build_script_build-9e29bfbcb08d01db.d: C:\Users\ccgauche\.cargo\registry\src\github.com-1ecc6299db9ec823\miniz_oxide-0.4.4\build.rs
-
-C:\Users\ccgauche\.cargo\registry\src\github.com-1ecc6299db9ec823\miniz_oxide-0.4.4\build.rs:
--- raknet/target/debug/deps/autocfg-821acbf0e74ce2d6.d
+++
@@ -1,9 +0,0 @@
-C:\Users\ccgauche\Documents\GitHub\anvil_bedrock\raknet\target\debug\deps\autocfg-821acbf0e74ce2d6.rmeta: C:\Users\ccgauche\.cargo\registry\src\github.com-1ecc6299db9ec823\autocfg-1.0.1\src\lib.rs C:\Users\ccgauche\.cargo\registry\src\github.com-1ecc6299db9ec823\autocfg-1.0.1\src\error.rs C:\Users\ccgauche\.cargo\registry\src\github.com-1ecc6299db9ec823\autocfg-1.0.1\src\version.rs
-
-C:\Users\ccgauche\Documents\GitHub\anvil_bedrock\raknet\target\debug\deps\libautocfg-821acbf0e74ce2d6.rlib: C:\Users\ccgauche\.cargo\registry\src\github.com-1ecc6299db9ec823\autocfg-1.0.1\src\lib.rs C:\Users\ccgauche\.cargo\registry\src\github.com-1ecc6299db9ec823\autocfg-1.0.1\src\error.rs C:\Users\ccgauche\.cargo\registry\src\github.com-1ecc6299db9ec823\autocfg-1.0.1\src\version.rs
-
-C:\Users\ccgauche\Documents\GitHub\anvil_bedrock\raknet\target\debug\deps\autocfg-821acbf0e74ce2d6.d: C:\Users\ccgauche\.cargo\registry\src\github.com-1ecc6299db9ec823\autocfg-1.0.1\src\lib.rs C:\Users\ccgauche\.cargo\registry\src\github.com-1ecc6299db9ec823\autocfg-1.0.1\src\error.rs C:\Users\ccgauche\.cargo\registry\src\github.com-1ecc6299db9ec823\autocfg-1.0.1\src\version.rs
-
-C:\Users\ccgauche\.cargo\registry\src\github.com-1ecc6299db9ec823\autocfg-1.0.1\src\lib.rs:
-C:\Users\ccgauche\.cargo\registry\src\github.com-1ecc6299db9ec823\autocfg-1.0.1\src\error.rs:
-C:\Users\ccgauche\.cargo\registry\src\github.com-1ecc6299db9ec823\autocfg-1.0.1\src\version.rs:
